6c485fb7e8af30b424705017e9ecad865618c331
ronib <tyneworr@gmail.com>
2025-09-18 10:04:18 +0300

    18.09 test commit

--- src/main/java/tregulovTest/ScopeTest.java
+++ src/main/java/tregulovTest/ScopeTest.java
@@ -9,6 +9,7 @@ public class ScopeTest {
                 new ClassPathXmlApplicationContext("applicationContext3.xml");
         Dog dog1 = context.getBean("dog", Dog.class);
         dog1.say();
+        dog1.say();
 //        Dog dog2 = context.getBean("dog", Dog.class);
 //        System.out.println("dogi ravni? " + (dog1 == dog2));
 //        System.out.println(dog1);
